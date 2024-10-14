3dbea0baab87323e77800a300c00afdb64561d10
Laodice <laodice.melliti@gmail.com>
2024-10-14 22:45:37 +0200

    Display the occurrences count and add tests

--- app/src/main/java/com/example/wikipediatopics/MainActivity.kt
+++ app/src/main/java/com/example/wikipediatopics/MainActivity.kt
@@ -5,6 +5,7 @@ import android.view.ViewGroup
 import android.webkit.WebView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.VisibleForTesting
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -12,9 +13,11 @@ import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
@@ -23,6 +26,8 @@ import com.example.wikipediatopics.ui.theme.WikipediaTopicsTheme
 import org.koin.androidx.compose.koinViewModel
 
 const val TEST_TAG_SEARCH_BAR = "TEST_TAG_SEARCH_BAR"
+const val TEST_TAG_QUERY_OCCURRENCES = "TEST_TAG_QUERY_OCCURRENCES"
+const val TEST_TAG_WEB_PAGE = "TEST_TAG_WEB_PAGE"
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -43,6 +48,10 @@ fun App(viewModel: PageViewModel = koinViewModel()) {
     val isSearching by viewModel.isSearching.collectAsState()
     val wikiPage by viewModel.wikiPage.collectAsState()
 
+    val queryOccurrences by derivedStateOf {
+        wikiPage?.text?.htmlDump?.countOccurrencesOf(query) ?: 0
+    }
+
     SearchBar(
         query = query,
         onQueryChange = viewModel::onQueryChange,
@@ -56,8 +65,12 @@ fun App(viewModel: PageViewModel = koinViewModel()) {
                 .testTag(TEST_TAG_SEARCH_BAR),
     ) {
         wikiPage?.let { page ->
-            Text(text = page.title)
+            Text(
+                stringResource(id = R.string.wiki_page_query_occurrences, page.title, queryOccurrences),
+                modifier = Modifier.testTag(TEST_TAG_QUERY_OCCURRENCES),
+            )
             AndroidView(
+                modifier = Modifier.testTag(TEST_TAG_WEB_PAGE),
                 factory = { viewContext ->
                     WebView(viewContext).apply {
                         layoutParams =
@@ -74,6 +87,9 @@ fun App(viewModel: PageViewModel = koinViewModel()) {
         }
     }
 }
+
+@VisibleForTesting
+fun String.countOccurrencesOf(word: String): Int = word.toRegex(RegexOption.IGNORE_CASE).findAll(this, 0).count()
 
 @Preview(showBackground = true)
 @Composable
--- app/src/test/java/com/example/wikipediatopics/MainActivityTests.kt
+++ app/src/test/java/com/example/wikipediatopics/MainActivityTests.kt
@@ -7,12 +7,19 @@ import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onAllNodesWithTag
 import androidx.compose.ui.test.onChild
 import androidx.compose.ui.test.onFirst
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performImeAction
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.wikipediatopics.api.WikipediaArticle
+import com.example.wikipediatopics.api.WikipediaPage
 import com.example.wikipediatopics.page.PageViewModel
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.verify
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.core.Is.`is`
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
@@ -61,5 +68,65 @@ class MainActivityTests : KoinTest {
                 onFirst().onChild().assertTextEquals("value")
             }
         }
+    }
+
+    @Test
+    fun `Entering a topic and clicking enter should start a search`() {
+        every { viewModel.query } returns MutableStateFlow("Query").asStateFlow()
+        every { viewModel.isSearching } returns MutableStateFlow(true).asStateFlow()
+        every { viewModel.wikiPage } returns MutableStateFlow(null).asStateFlow()
+
+        with(composeRule) {
+            setContent { App() }
+
+            onNodeWithTag(TEST_TAG_SEARCH_BAR).onChild().apply {
+                performImeAction()
+            }
+
+            verify(exactly = 1) { viewModel.onSearch("Query") }
+        }
+    }
+
+    @Test
+    fun `Should display wikipedia page and number of occurrences when searching for a topic`() {
+        every { viewModel.query } returns MutableStateFlow("value").asStateFlow()
+        every { viewModel.isSearching } returns MutableStateFlow(true).asStateFlow()
+        every { viewModel.wikiPage } returns
+            MutableStateFlow(
+                WikipediaPage(
+                    title = "title",
+                    pageId = 1234,
+                    text = WikipediaArticle("<h1>html dump</h1>"),
+                ),
+            ).asStateFlow()
+
+        with(composeRule) {
+            setContent { App() }
+
+            onAllNodesWithTag(TEST_TAG_QUERY_OCCURRENCES).apply {
+                assertCountEquals(1)
+                onFirst().assertIsDisplayed()
+            }
+            onAllNodesWithTag(TEST_TAG_WEB_PAGE).apply {
+                assertCountEquals(1)
+                onFirst().assertExists()
+            }
+        }
+    }
+
+    @Test
+    fun `Should find the matching word in the text regardless of letter case`() {
+        val text = "AQuerylongquerytextcontaiQueryningQuerywordsqueryIwanttocountQuery"
+        val word = "Query"
+
+        assertThat(text.countOccurrencesOf(word), `is`(6))
+    }
+
+    @Test
+    fun `Should return 0 if there's no instances of the word`() {
+        val text = "AlongtextcontainstardewingwordsIwanttocount"
+        val word = "Query"
+
+        assertThat(text.countOccurrencesOf(word), `is`(0))
     }
 }
